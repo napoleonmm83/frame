576cbcfebb4f6f23b1a4ab80961dd2d4bd16d628
Marcus Martini <marcusmartini83@gmail.com>
2020-10-31 18:35:42 +0100

    fix

---
+++ config.js
@@ -0,0 +1,11 @@
+let uid = 'marcusmartini83@gmail.com';
+var serviceAccount = require("./my-piframe-firebase-adminsdk-57dmi-6ffc02f3b5.json");
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA3oQk3_wKpMJP2NHeBtY0QYH5cLQlzPbM",
+    authDomain: "my-piframe.firebaseapp.com",
+    databaseURL: "https://my-piframe.firebaseio.com",
+    projectId: "my-piframe",
+    storageBucket: "my-piframe.appspot.com",
+};
