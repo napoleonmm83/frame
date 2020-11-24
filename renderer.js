6a2c2de42c52ed171625accabe7d3d9d0942791e
napoleonmm83 <marcusmartini83@gmail.com>
2020-11-24 22:08:36 +0100

    update fadin fadout

--- renderer.js
+++ renderer.js
@@ -10,77 +10,73 @@
 
 
 
+
+
+
+
+
+
 var myVar = setInterval(myTimer, 10000);
 
-
-
-
-
 function myTimer() {
-var fs = require('fs');
-var dirPath = './images/';
-let  content = "";
-fs.readdir(dirPath, (err, dir)=>{
-      var array=[];
-  for(var i=0; i<dir.length; i++){
-      let fileName = dir[i];
-    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-    if(!allowedExtensions.exec(fileName)){
 
-  }else{
+  let p = new Promise((resolve, reject) => {
+    var fs = require('fs');
+    var dirPath = './images/';
+    let  content = "";
+    fs.readdir(dirPath, (err, dir)=>{
+          var array=[];
+      for(var i=0; i<dir.length; i++){
+          let fileName = dir[i];
+        var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+        if(!allowedExtensions.exec(fileName)){
 
-
-
-      let filePath=dirPath+"/"+fileName;
-     // console.log("A: "+fileName);
-      array.push(fileName);
-}
-  }
-console.log(array);
+      }else{
 
 
-i = Math.floor(Math.random()*array.length);
-var url = (dirPath+array[i]);
 
-console.log(url);
-
-let image = new Image();
-image.onload = function () {
- console.log ("pic Height: " + this.naturalHeight);
- console.log ("pic Width: " + this.naturalWidth);
- // Mach was mit dem Bild
-};
-image.src = url;
-var picdiv = document.getElementById('frame');
-picdiv.classList.remove("fade-in");
-picdiv.classList.add('fade-out');
-unfade(picdiv,url)
-//fade(picdiv,url)
-
-/*
-function unfade(element,url) {
-    var op = 0.1;  // initial opacity
-
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.backgroundImage="url('"+url+"')";
-        element.style.backgroundSize ="contain";
-        element.style.backgroundRepeat="no-repeat";
-        element.style.backgroundPosition = "center center";
-        element.style.opacity =  op * 1 ;
-        element.style.transition = "background 4s ease-in";
-        op += op * 0.1;
-    }, 50);
-}
-*/
+          let filePath=dirPath+"/"+fileName;
+         // console.log("A: "+fileName);
+          array.push(fileName);
+    }
+      }
+    console.log(array);
 
 
-function unfade(element,url) {
-picdiv.classList.remove("fade-out");
+    i = Math.floor(Math.random()*array.length);
+    var url = (dirPath+array[i]);
+
+    console.log(url);
+
+    let image = new Image();
+    image.onload = function () {
+     console.log ("pic Height: " + this.naturalHeight);
+     console.log ("pic Width: " + this.naturalWidth);
+     // Mach was mit dem Bild
+    };
+    image.src = url;
+          resolve(url);
+})
+  });
+
+  p.then((url) => {
+
+      setTimeout(() => {
+          var element= document.getElementById('frame');
+
+          element.classList.remove("fade-in");
+          void element.offsetWidth;
+            element.classList.add('fade-out');
+
+      }, 2000);
+      return url;
+  }).then((url) => {
+    setTimeout(() => {
+        console.log(url);
+var element= document.getElementById('frame');
+
+element.classList.remove("fade-out");
+void element.offsetWidth;
   element.classList.add('fade-in');
     element.style.display = 'block';
 
@@ -91,9 +87,14 @@ picdiv.classList.remove("fade-out");
         element.style.backgroundPosition = "center center";
         element.style.animation = "animation: fadeInFromNone 3.5s ease-in";
 
-}
 
-})
+    }, 3000);
+
+      return url;
+  });
+
+
+
 }
 
 
